cb40cccd5d81f152eb98cf589c1760b4b8a873d7
jubaer997 <syedjubaer997@gmail.com>
2020-05-28 21:15:51 +0600

    all files uploaded

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,28 @@
+class DemoController < ApplicationController
+
+  layout false
+
+  def index
+    @subjects = Subject.all
+  end
+
+  def hello
+    @array = [10, 20, 30, 40]
+  end
+
+  def about
+    render('about_us')
+  end
+
+  def contact
+    if ['us', 'ca'].include?(params[:country])
+      @phone = "(800) 555-6789"
+    elsif params[:country] == 'uk'
+      @phone = "(020) 7946 1234"
+    else
+      @phone = "+1 (987) 654-3210"
+    end
+    render('contact_us')
+  end
+
+end
